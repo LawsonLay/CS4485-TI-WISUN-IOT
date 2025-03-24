457a25daab17b476b388aa870075abecf374b9c0
kvv190001 <kvv190001@utdallas.edu>
2025-03-24 16:21:12 -0500

    Implemented Stork Web Page

---
+++ ti-wisunfantund/ti-wisun-webapp/client/src/components/StorkTab.tsx
@@ -0,0 +1,8 @@
+import '../App.css';
+import React, { useContext, useState, useRef, useEffect } from 'react';
+
+export default function StorkTab() {
+  return (
+    <iframe src="http://localhost:8080/" style={{ width: '100%', height: '70vh' }}></iframe>
+  );
+}

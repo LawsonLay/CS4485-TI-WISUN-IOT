ad6bb7d9da28091bb7ec9896349a2ea1cce2b9fc
kvv190001 <kvv190001@utdallas.edu>
2025-03-25 12:36:02 -0500

    fixed minor display issues on stork page

--- ti-wisunfantund/ti-wisun-webapp/client/src/components/StorkTab.tsx
+++ ti-wisunfantund/ti-wisun-webapp/client/src/components/StorkTab.tsx
@@ -3,6 +3,6 @@ import React, { useContext, useState, useRef, useEffect } from 'react';
 
 export default function StorkTab() {
   return (
-    <iframe src="http://localhost:8080/" style={{ width: '100%', height: '70vh' }}></iframe>
+    <iframe src="http://localhost:8080/" style={{ width: '100%', height: '88vh', marginTop: '-20px', position: 'fixed' }}></iframe>
   );
 }
